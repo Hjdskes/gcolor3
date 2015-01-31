f93796d30e36a9f28ad0afae96f99e8211848c54
Jente Hidskes <hjdskes@gmail.com>
2015-01-31 23:17:45 +0100

    Update build system
    This time we write them ourselves, instead of using some half-Anjuta generated
    files.

---
+++ common.mk
@@ -0,0 +1,8 @@
+M_CFLAGS = -fstack-protector -Wall -pedantic \
+	-Wstrict-prototypes -Wundef -fno-common \
+	-Werror-implicit-function-declaration \
+	-Wformat -Wformat-security -Werror=format-security \
+	-Wno-conversion -Wunreachable-code -std=c99 -Werror \
+	-DDATADIR=\"$(datadir)\"
+
+AM_CPPFLAGS += -I $(top_srcdir)/src
