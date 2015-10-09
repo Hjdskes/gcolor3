35d7afe41e43e337bf1ab8d43ebc8167383c23dd
Jente Hidskes <hjdskes@gmail.com>
2015-10-09 23:00:50 +0200

    build: PKG_NAME is now deprecated

--- autogen.sh
+++ autogen.sh
@@ -4,8 +4,6 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="gcolor3"
-
 (test -f $srcdir/configure.ac) || {
 	echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
 	echo "top-level $PKG_NAME directory."
