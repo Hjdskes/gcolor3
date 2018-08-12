836c49b774a994a96fd04106d02322e9a157120d
Jente Hidskes <hjdskes@gmail.com>
2018-08-12 21:32:08 +0200

    meson install: update the desktop database

--- meson_install.sh
+++ meson_install.sh
@@ -3,6 +3,9 @@
 if [ -z $DESTDIR ]; then
   DATADIR=$1
 
+  # Update the desktop database
+  update-desktop-database -q
+
   # Update icon cache
   gtk-update-icon-cache -f -t $MESON_INSTALL_DESTDIR_PREFIX/$DATADIR/icons/hicolor
 fi
